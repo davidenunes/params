4bf565756babf3154519cc2d23ec5708c32a0d16
Davide Nunes <davidelnunes@gmail.com>
2017-12-03 20:23:28 +0000

    simple param space library started

---
+++ test/test_params.py
@@ -0,0 +1,30 @@
+import unittest
+from params import ParamSpace
+import contextlib
+
+
+class MyTestCase(unittest.TestCase):
+    def test_param_grid(self):
+        ps = ParamSpace()
+        ps.add_value("p1", True)
+        ps.add_list("p2", ["A", "B"])
+        ps.add_random("p3", 0, 1, 3)
+        print("param space size ", ps.grid_size)
+
+
+        grid = ps.param_grid()
+
+
+        for params in grid:
+            print(params)
+
+        grid = ps.param_grid()
+        grid = list(grid)
+        self.assertEqual(len(grid), 1 * 2 * 3)
+        self.assertEqual(len(grid), ps.grid_size)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
